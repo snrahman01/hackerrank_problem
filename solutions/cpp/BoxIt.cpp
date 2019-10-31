74a1f043db1dbd15ed0102bafcdf8492e982fb62
snrahman01 <44856519+snrahman01@users.noreply.github.com>
2019-10-31 15:27:12 +0600

    Update BoxIt.cpp

--- solutions/cpp/BoxIt.cpp
+++ solutions/cpp/BoxIt.cpp
@@ -1,27 +1,6 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-//Implement the class Box
-//l,b,h are integers representing the dimensions of the box
-
-// The class should have the following functions :
-
-// Constructors:
-// Box();
-// Box(int,int,int);
-// Box(Box);
-
-
-// int getLength(); // Return box's length
-// int getBreadth (); // Return box's breadth
-// int getHeight ();  //Return box's height
-// long long CalculateVolume(); // Return the volume of the box
-
-//Overload operator < as specified
-//bool operator<(Box& b)
-
-//Overload operator << as specified
-//ostream& operator<<(ostream& out, Box&
 class Box{
 public:
     Box(){
